dbf2f92b41b90586f0df69f7490819b4c0742d9b
hfr4 <30574115+hfr4@users.noreply.github.com>
2023-10-25 14:12:25 +0200

    add reunion component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReunionComponent } from './reunion/reunion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReunionComponent
   ],
   imports: [
     BrowserModule,
