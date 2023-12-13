8bd76292d76c828471c128eda5f0744596e19ec9
hfr4 <30574115+hfr4@users.noreply.github.com>
2023-12-13 13:33:38 +0100

    get params to routes

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
+
+    constructor(private route: ActivatedRoute) {}
+
+    my_id: string = "";
+
+    ngOnInit() {
+        this.route.paramMap.subscribe(params => {
+            let id = params.get('id');
+            if (id) {
+                this.my_id = id;
+            }
+            
+            // this.product = this.products.find(product => product.id === +id);
+        })
+    }
+
 }
