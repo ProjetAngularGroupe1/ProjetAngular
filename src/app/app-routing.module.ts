14fb8c056f98b5a253b68d5530b73b7341da00e9
selma <salma.ayachi@insat.u-carthage.tn>
2023-12-13 11:21:34 +0100

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { ArticleComponent } from './components/article/article.component';
+import { CommentComponent } from './components/comment/comment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ArticleListComponent  },
+  {path: 'articles', component:ArticleListComponent },
+  {path: 'articles/:id', component: ArticleComponent},
+  {path: 'comments/:id',component: CommentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
